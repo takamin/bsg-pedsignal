198b5544dcf999394ea7c2855581b7d33a61fccb
Koji Takami <vzg03566@gmail.com>
2015-06-08 10:31:07 +0000

    Initial commit

---
+++ setupdaemon.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+NAME=pedsignal
+DSCR=/etc/init.d/${NAME}d
+DEXE=/usr/local/sbin/${NAME}d
+PIDF=/var/run/${NAME}d.pid
+
+sudo ${DSCR} stop
+
+sudo cp ./pedsignald.sh ${DSCR}
+sudo chown root.root ${DSCR}
+sudo chmod 755 ${DSCR}
+
+sudo cp build/pedsignal ${DEXE}
+sudo chown root.root ${DEXE}
+
+sudo update-rc.d ${NAME}d defaults
+
+sudo ${DSCR} start
